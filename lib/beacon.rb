f90445ec27150e97f25961888ffa4489d1d1b12c
Zach Grande <zach.grande23@gmail.com>
2023-11-22 10:41:34 -0800

    Initial commit

---
+++ lib/beacon.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "beacon/version"
+
+module Beacon
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/beacon/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Beacon
+  VERSION = "0.1.0"
+end
